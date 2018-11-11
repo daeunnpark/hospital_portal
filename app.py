15e13366f1d482db77561e89d34db5c5377fd141
Thomas Vetere <thomas.vetere@stonybrook.edu>
2018-11-11 18:33:35 -0500

    "added icon"

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 import pymysql
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
 
-def main():
 
-    app = QApplication(sys.argv)
+class Application(QWidget):
 
-    root = QWidget()
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-    root.resize(320, 240)
+    def initUI(self):
 
-    root.setWindowTitle("Hello, World")
+        self.setGeometry(300, 300, 300, 220)
 
-    root.show()
+        self.setWindowTitle("HealthCare United Patient Portal")
+
+        self.setWindowIcon(QIcon('icon.PNG'))
+
+        self.show()
+
+
+if __name__ == "__main__":
 
     conn = pymysql.connect(host='10.245.235.98', port=3306, user='root', passwd='hospitalCSE305!', db='hospital')
 
     cur = conn.cursor()
 
-    cur.execute("SELECT VERSION()")
+    app = QApplication(sys.argv)
 
-    print(cur.description)
+    root = Application()
 
     sys.exit(app.exec_())
-
-if __name__ == "__main__": main()
