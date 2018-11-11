d6eff697b88a0bea9fbe8aef5705e9376cfcf0ce
Thomas Vetere <thomas.vetere@stonybrook.edu>
2018-11-11 18:02:25 -0500

    test of python window

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-#!/usr/bin/env python
 import pymysql
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
 
 def main():
+
+    app = QApplication(sys.argv)
+
+    root = QWidget()
+
+    root.resize(320, 240)
+
+    root.setWindowTitle("Hello, World")
+
+    root.show()
 
     conn = pymysql.connect(host='10.245.235.98', port=3306, user='root', passwd='hospitalCSE305!', db='hospital')
 
@@ -10,5 +21,7 @@ def main():
     cur.execute("SELECT VERSION()")
 
     print(cur.description)
+
+    sys.exit(app.exec_())
 
 if __name__ == "__main__": main()
