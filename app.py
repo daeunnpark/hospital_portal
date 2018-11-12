1693120cb68f434d4786d7fe1bef95048b8d7299
Thomas Vetere <thomas.vetere@stonybrook.edu>
2018-11-11 19:09:27 -0500

    commit

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 import pymysql
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QDesktopWidget, QWidget, QPushButton, QToolTip, QMessageBox
+from PyQt5.QtGui import QIcon, QFont
 
 
 class Application(QWidget):
@@ -14,13 +14,59 @@ class Application(QWidget):
 
     def initUI(self):
 
+        QToolTip.setFont(QFont('SansSerif', 10))
+
+        patientButton = QPushButton('Patient', self)
+
+        doctorButton = QPushButton('Doctor', self)
+
+        nurseButton = QPushButton('Nurse', self)
+
+        adminButton = QPushButton('Administrator', self)
+
+        patientButton.setToolTip('Select this if you are a patient')
+
+        doctorButton.setToolTip('Select this if you are a doctor')
+
+        nurseButton.setToolTip('Select this if you are a nurse')
+
+        adminButton.setToolTip('Select this if you are an administrator')
+
+        patientButton.resize(patientButton.sizeHint())
+
+        doctorButton.resize(doctorButton.sizeHint())
+
+        nurseButton.resize(nurseButton.sizeHint())
+
+        adminButton.resize(adminButton.sizeHint())
+
         self.setGeometry(300, 300, 300, 220)
+
+        self.center()
 
         self.setWindowTitle("HealthCare United Patient Portal")
 
         self.setWindowIcon(QIcon('icon.PNG'))
 
         self.show()
+
+    def center(self):
+
+        qr = self.frameGeometry();
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def closeEvent(self, event):
+
+        reply = QMessageBox.question(self, 'Message', "Are you sure you want to quit?",
+            QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+
+        else:
+            event.ignore()
 
 
 if __name__ == "__main__":
