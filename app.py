699a733a6b8ef33a834c8e29454330e2716858b6
rhassett2 <30306126+rhassett2@users.noreply.github.com>
2018-11-17 17:26:43 -0500

    Added Ui for Signing in and Registering

--- app.py
+++ app.py
@@ -15,14 +15,15 @@ class Application(QMainWindow):
     def __init__(self):
         super().__init__()
         #Load UI File: To Edit UI File, Open File .ui file in QtDesigner
-        uic.loadUi('mainwindow.ui', self)
+        #uic.loadUi('mainwindow.ui', self)
+        uic.loadUi('signinorregister.ui', self)
         #uic.loadUi('menu.ui',self) # comment out line#47 too to run menu.ui
         #The widget names are listed in the right panel of QtDesigner. They will need to be used to call them and implement them in python
         self.initUI()
 
     #Function that centers the window in the display upon loading
     def center(self):
-        qr = self.frameGeometry();
+        qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
