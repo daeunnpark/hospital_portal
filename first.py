c006c77d6189b98769a91b7065571eba335891a0
rhassett2 <30306126+rhassett2@users.noreply.github.com>
2018-11-23 14:58:34 -0500

    Insert Statement Not Updating Database

--- first.py
+++ first.py
@@ -736,8 +736,10 @@ class Ui_CommonSignUp(object):
         self.pushButton.setText(_translate("Main Window", "Sign Up!"))
 
     def CreatePatient(self, access):
+        if(self.lineEdit_6.text() == "" or self.lineEdit_7.text() == "" or self.lineEdit_5.text() == ""):
+            print("error empty")
         if(self.lineEdit_6.text() != self.lineEdit_7.text()):
-            print("error")
+            print("error not same")
         else:
             cur.execute('SELECT * FROM Person P WHERE Username = (%s)', (self.lineEdit_5.text()))
             if(cur.rowcount != 0):
@@ -746,8 +748,8 @@ class Ui_CommonSignUp(object):
                 cur.execute('SELECT * FROM Person P WHERE UserPassword = (%s)', (self.lineEdit_6.text()))
                 if(cur.rowcount != 0):
                     print("error: UserPassword Already Exists")
-                #else:
-                    #cur.execute('INSERT INTO Person(ID, FirstName, LastName, PhoneNumber, EmailAddress, Username, UserPassword) VALUES ( access, self.lineEdit, self.lineEdit_2, self.lineEdit_3, self.lineEdit_4, self.lineEdit_5, self.lineEdit_6)')
+                else:
+                    cur.execute('INSERT INTO Person(ID, FirstName, LastName, PhoneNumber, EmailAddress, Username, UserPassword) VALUES (2, "f", "1234567891", "h", "i", "j", "k")')
 
 
 if __name__ == "__main__":
