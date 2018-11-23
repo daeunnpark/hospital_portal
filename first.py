393d478c9361851f51a8264f32e68c6ce6c5017c
daeunnpark <daeunn@gmail.com>
2018-11-23 11:16:09 -0500

    pymysql connect command changed localhost -> ip addr

--- first.py
+++ first.py
@@ -17,7 +17,7 @@ import pymysql
 class login_page(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        #MainWindow.resize(800, 600)
+        MainWindow.resize(800, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.imageLabel = QtWidgets.QLabel(self.centralwidget)
@@ -654,8 +654,9 @@ if __name__ == "__main__":
     import sys
     # Initialize database connection
     #conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='root', db='hospital')
+    conn = pymysql.connect(host='10.245.235.98', port=3306, user='root', passwd='hospitalCSE305!', db='hospital')
     # Initialize the database cursor
-    #cur = conn.cursor()
+    cur = conn.cursor()
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = login_page() # set login page as UI
