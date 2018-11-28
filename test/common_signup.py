937b8d3360913b48a0e93c7a7849030ede14b284
daeunnpark <daeunn@gmail.com>
2018-11-28 00:04:24 -0500

    phone nb checking including dashes fixed 11->12 letters

--- test/common_signup.py
+++ test/common_signup.py
@@ -204,17 +204,19 @@ class common_signup_UI(object):
         self.label_7.setText(_translate("MainWindow", "Re-Enter Password:"))
         self.label_5.setText(_translate("MainWindow", "Username:"))
         self.label_6.setText(_translate("MainWindow", "Password:"))
+
         if(num == 1):
-            self.label_9.setText(_translate("MainWindow", "Weight:"))
             self.label_8.setText(_translate("MainWindow", "Age:"))
+            self.label_9.setText(_translate("MainWindow", "Weight:"))
         elif(num == 2 or num == 3):
             self.label_8.setText(_translate("MainWindow", "Specialty:"))
             self.label_9.setText(_translate("MainWindow", "Medical License:"))
+
         self.label_12.setText(_translate("MainWindow", "Credit Card Number:"))
         self.label_13.setText(_translate("MainWindow", "Insurance Number:"))
         self.pushButton.setText(_translate("MainWindow", "Sign In!"))
 
-    #second arg access removed - unused
+
     def CreatePatient(self, cur, conn, accessCodeReceived):
         self.newP = False
         if(self.lineEdit_6.text() == "" or self.lineEdit_7.text() == "" or self.lineEdit_5.text() == ""):
@@ -238,62 +240,19 @@ class common_signup_UI(object):
                     error_dialog.setText("Error: Password Already Exists! Try Another Password")
                     error_dialog.exec()
                 else:
-                    if(len(self.lineEdit_3.text()) != 12 or self.lineEdit_3.text()[3] != '-' or self.lineEdit_3.text()[7] != '-' or self.lineEdit_3.text()[0].isdigit() == False
-                        or self.lineEdit_3.text()[1].isdigit() == False or self.lineEdit_3.text()[2].isdigit() == False or self.lineEdit_3.text()[4].isdigit() == False
-                        or self.lineEdit_3.text()[5].isdigit() == False or self.lineEdit_3.text()[6].isdigit() == False or self.lineEdit_3.text()[8].isdigit() == False
-                        or self.lineEdit_3.text()[9].isdigit() == False or self.lineEdit_3.text()[10].isdigit() == False or self.lineEdit_3.text()[11].isdigit() == False) :
+                    if(len(self.lineEdit_3.text()) != 11 or self.lineEdit_3.text()[3] != '-' or self.lineEdit_3.text()[7] != '-') :
                         error_dialog = QtWidgets.QMessageBox()
                         error_dialog.setText("Error: Phone Number Incorrect! Format: xxx-xxx-xxxx")
                         error_dialog.exec()
-                    elif(len(self.lineEdit_11.text()) != 11 or self.lineEdit_11.text()[3] != '-' or self.lineEdit_11.text()[6] != '-'
-                        or self.lineEdit_11.text()[0].isdigit() == False or self.lineEdit_11.text()[1].isdigit() == False or self.lineEdit_11.text()[2].isdigit() == False
-                        or self.lineEdit_11.text()[4].isdigit() == False or self.lineEdit_11.text()[5].isdigit() == False or self.lineEdit_11.text()[7].isdigit() == False
-                        or self.lineEdit_11.text()[8].isdigit() == False or self.lineEdit_11.text()[9].isdigit() == False or self.lineEdit_11.text()[10].isdigit() == False):
+                    elif(len(self.lineEdit_11.text()) != 9):
                         error_dialog = QtWidgets.QMessageBox()
-                        error_dialog.setText("Error: SSN Incorrect! Must be 9 numbers long! Format: xxx-xx-xxxx")
-                        error_dialog.exec()
-                    elif(self.lineEdit_8.text().isdigit() == False):
-                        error_dialog = QtWidgets.QMessageBox()
-                        error_dialog.setText("Error: Age Must Be A Number")
-                        error_dialog.exec()
-                    elif(self.lineEdit_9.text().replace('.', '1').isdigit() == False):
-                        error_dialog = QtWidgets.QMessageBox()
-                        error_dialog.setText("Error: Weight Must Be A Number")
-                        error_dialog.exec()
-                    elif(self.lineEdit_10.text().replace('.', '1').isdigit() == False):
-                        error_dialog = QtWidgets.QMessageBox()
-                        error_dialog.setText("Error: Height Must Be A Number")
-                        error_dialog.exec()
-                    elif(self.lineEdit_12.text().isdigit() == False):
-                        error_dialog = QtWidgets.QMessageBox()
-                        error_dialog.setText("Error: Credit Card Number Must Be A Number")
-                        error_dialog.exec()
-                    elif(self.lineEdit_13.text().isdigit() == False):
-                        error_dialog = QtWidgets.QMessageBox()
-                        error_dialog.setText("Error: Insurance Number Must Be A Number")
+                        error_dialog.setText("Error: SSN Incorrect! Must be 9 numbers long")
                         error_dialog.exec()
                     else:
-                        number = self.lineEdit_3.text()
-                        for char in number:
-                            if char in "-":
-                                number = number.replace(char, '')
-                        number2 = self.lineEdit_11.text()
-                        for char2 in number2:
-                            if char2 in "-":
-                                number2 = number2.replace(char2, '')
-                        cur.execute('INSERT INTO Person(ID, FirstName, LastName, PhoneNumber, EmailAddress, Username, UserPassword) VALUES (%s, %s, %s, %s, %s, %s, %s)', (accessCodeReceived, self.lineEdit.text(), self.lineEdit_2.text(), number, self.lineEdit_4.text(), self.lineEdit_5.text(), self.lineEdit_6.text()))
-                    
-                        cur.execute('INSERT INTO Patient(PatientID, Age, Weight, Height, SSN, CreditCardNumber, BillingAmount, InsuranceNumber, MedicationList) VALUES (%s, %s, %s, %s, %s, %s, 20.00, %s, "none")', (accessCodeReceived, self.lineEdit_8.text(), self.lineEdit_9.text(), self.lineEdit_10.text(), number2, self.lineEdit_12.text(), self.lineEdit_13.text()))
-                        # Line above : replaced access -> "305" to avoid using access
+                        cur.execute('INSERT INTO Person(ID, FirstName, LastName, PhoneNumber, EmailAddress, Username, UserPassword) VALUES (%s, %s, %s, %s, %s, %s, %s)', (accessCodeReceived, self.lineEdit.text(), self.lineEdit_2.text(), self.lineEdit_3.text(), self.lineEdit_4.text(), self.lineEdit_5.text(), self.lineEdit_6.text()))
+                        cur.execute('INSERT INTO Patient(PatientID, Age, Weight, Height, SSN, CreditCardNumber, BillingAmount, InsuranceNumber, MedicationList) VALUES (%s, %s, %s, %s, %s, %s, 20.00, %s, "none")', (accessCodeReceived, self.lineEdit_8.text(), self.lineEdit_9.text(), self.lineEdit_10.text(), self.lineEdit_11.text(), self.lineEdit_12.text(), self.lineEdit_13.text()))
                         conn.commit()
                         self.newP = True
-                    # moved to test.py
-
-                    """conn.commit()
-                    self.uiLogin = common_login_UI()
-                    self.uiLogin.setupUi(MainWindow)
-                    MainWindow.showMaximized()
-                    """
 
     def CreateDoctor(self, cur, conn, accessCodeReceived):
         self.newD = False
@@ -318,33 +277,20 @@ class common_signup_UI(object):
                     error_dialog.setText("Error: Password Already Exists! Choose Another Password")
                     error_dialog.exec()
                 else:
-                    if (len(self.lineEdit_3.text()) != 12 or self.lineEdit_3.text()[3] != '-' or self.lineEdit_3.text()[7] != '-' or self.lineEdit_3.text()[0].isdigit() == False
-                        or self.lineEdit_3.text()[1].isdigit() == False or self.lineEdit_3.text()[2].isdigit() == False or self.lineEdit_3.text()[4].isdigit() == False
-                        or self.lineEdit_3.text()[5].isdigit() == False or self.lineEdit_3.text()[6].isdigit() == False or self.lineEdit_3.text()[8].isdigit() == False
-                        or self.lineEdit_3[9].text() == False or self.lineEdit_3[10].text() == False or self.lineEdit_3[11].text() == False):
+                    if (len(self.lineEdit_3.text()) != 12 or self.lineEdit_3.text()[3] != '-' or self.lineEdit_3.text()[
+                        7] != '-'):
                         error_dialog = QtWidgets.QMessageBox()
                         error_dialog.setText("Error: Phone Number Incorrect! Format: xxx-xxx-xxxx")
                         error_dialog.exec()
                     else:
-                        number = self.lineEdit_3.text()
-                        for char in number:
-                            if char in "-":
-                                number = number.replace(char, '')
-                        cur.execute('INSERT INTO Person(ID, FirstName, LastName, PhoneNumber, EmailAddress, Username, UserPassword) VALUES (%s, %s, %s, %s, %s, %s, %s)', (accessCodeReceived, self.lineEdit.text(), self.lineEdit_2.text(), number, self.lineEdit_4.text(), self.lineEdit_5.text(), self.lineEdit_6.text()))
+                        cur.execute('INSERT INTO Person(ID, FirstName, LastName, PhoneNumber, EmailAddress, Username, UserPassword) VALUES (%s, %s, %s, %s, %s, %s, %s)', (accessCodeReceived, self.lineEdit.text(), self.lineEdit_2.text(), self.lineEdit_3.text(), self.lineEdit_4.text(), self.lineEdit_5.text(), self.lineEdit_6.text()))
                         conn.commit()
                         cur.execute('INSERT INTO Employee(EmployeeID, DepartmentID) VALUES (%s, %s)', (accessCodeReceived, 1))
                         conn.commit()
                         cur.execute('INSERT INTO Doctor(DoctorID, Specialty, MedicalLicense) VALUES (%s, %s, %s)', (accessCodeReceived, self.lineEdit_8.text(), self.lineEdit_9.text()))
-                        # Line above : replaced access -> "305" to avoid using access
                         conn.commit()
                         self.newD = True
-                    # moved to test.py
-                    """
-                    conn.commit()
-                    self.uiLogin = common_login_UI()
-                    self.uiLogin.setupUi(MainWindow)
-                    MainWindow.showMaximized()
-                    """
+
 
     def CreateNurse(self, cur, conn, accessCodeReceived):
         self.newN = False
@@ -369,34 +315,19 @@ class common_signup_UI(object):
                     error_dialog.setText("Error: Password Already Exists! Choose Another One")
                     error_dialog.exec()
                 else:
-                    if (len(self.lineEdit_3.text()) != 12 or self.lineEdit_3.text()[3] != '-' or self.lineEdit_3.text()[7] != '-' or self.lineEdit_3.text()[0].isdigit() == False
-                        or self.lineEdit_3.text()[1].isdigit() == False or self.lineEdit_3.text()[2].isdigit() == False or self.lineEdit_3.text()[4].isdigit() == False
-                        or self.lineEdit_3.text()[5].isdigit() == False or self.lineEdit_3.text()[6].isdigit() == False or self.lineEdit_3.text()[8].isdigit() == False
-                        or self.lineEdit_3[9].text() == False or self.lineEdit_3[10].text() == False or self.lineEdit_3[11].text() == False):
+                    if (len(self.lineEdit_3.text()) != 12 or self.lineEdit_3.text()[3] != '-' or self.lineEdit_3.text()[
+                        7] != '-'):
                         error_dialog = QtWidgets.QMessageBox()
                         error_dialog.setText("Error: Phone Number Incorrect! Format: xxx-xxx-xxxx")
                         error_dialog.exec()
                     else:
-                        number = self.lineEdit_3.text()
-                        for char in number:
-                            if char in "-":
-                                number = number.replace(char, '')
-                        cur.execute('INSERT INTO Person(ID, FirstName, LastName, PhoneNumber, EmailAddress, Username, UserPassword) VALUES (%s, %s, %s, %s, %s, %s, %s)', (accessCodeReceived, self.lineEdit.text(), self.lineEdit_2.text(), number, self.lineEdit_4.text(), self.lineEdit_5.text(), self.lineEdit_6.text()))
+                        cur.execute('INSERT INTO Person(ID, FirstName, LastName, PhoneNumber, EmailAddress, Username, UserPassword) VALUES (%s, %s, %s, %s, %s, %s, %s)', (accessCodeReceived, self.lineEdit.text(), self.lineEdit_2.text(), self.lineEdit_3.text(), self.lineEdit_4.text(), self.lineEdit_5.text(), self.lineEdit_6.text()))
                         conn.commit()
                         cur.execute('INSERT INTO Employee(EmployeeID, DepartmentID) VALUES (%s, %s)', (accessCodeReceived, 1))
                         conn.commit()
                         cur.execute('INSERT INTO Nurse(NurseID, Specialty, MedicalLicense) VALUES (%s, %s, %s)', (accessCodeReceived, self.lineEdit_8.text(), self.lineEdit_9.text()))
-                        # Line above : replaced access -> "305" to avoid using access
                         conn.commit()
                         self.newN = True
-                    # moved to test.py
-                    """
-                    conn.commit()
-                    self.uiLogin = common_login_UI()
-                    self.uiLogin.setupUi(MainWindow)
-                    MainWindow.showMaximized()
-                    """
-
 
 
 if __name__ == "__main__":
